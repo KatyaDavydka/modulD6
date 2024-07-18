051ba09f817e181d0d27cd85c97af073af8e9b2d
KatyaDavydka <167692122+KatyaDavydka@users.noreply.github.com>
2024-07-18 18:43:56 +0300

    Add files via upload

---
+++ NewsPortal/news/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView, DetailView
+
+
+class NewsList(ListView):
+    # Указываем модель, объекты которой мы будем выводить
+    model = Post
+    # Поле, которое будет использоваться для сортировки объектов
+    ordering = '-post_time'
+    # Указываем имя шаблона, в котором будут все инструкции о том,
+    # как именно пользователю должны быть показаны наши объекты
+    template_name = 'post_list.html'
+    # Это имя списка, в котором будут лежать все объекты.
+    # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
+    context_object_name = 'posts'
+
+
+class NewsDetail(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+    context_object_name = 'post'
